84394bd2ec320b97fa165fce153e76b86778ca78
Max Wofford <max@maxwofford.com>
2021-04-15 18:19:54 -0400

    Add demo API route & get prisma migration working with view

--- prisma/migrations/20210414210022_add_time_series_views/migration.sql
+++ prisma/migrations/20210414210022_add_time_series_views/migration.sql
@@ -1,11 +1,13 @@
 -- CreateView
-CREATE VIEW "five_min_ema_view" AS
+CREATE VIEW "FiveMinEmaView" AS
     SELECT
-        COUNT(*),
+        ROW_NUMBER() OVER(ORDER BY (SELECT 1)) AS "id",
+        AVG(average) AS "average",
+        slack_id,
         date_trunc('hour', created) + (
             ((date_part('minute', created) :: integer) / 10 :: integer) * 10 :: integer || ' minutes'
         ) :: interval AS ten_min_timestamp
     FROM
         "MovingAverage"
     GROUP BY
-        ten_min_timestamp;
+        ten_min_timestamp, slack_id;
