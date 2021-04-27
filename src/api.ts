e6d806847d1db713832543ec823ad4feb2831b2c
Zachary Fogg <me@zfo.gg>
2021-04-27 16:41:29 -0400

    made a nice api endpoint for @msw / @maxwofford

--- src/api.ts
+++ src/api.ts
@@ -63,3 +63,49 @@ receiver.router.get(`/api/convo/:id`, cors(), (req: Request, res: Response, next
   }
 })
 
+const ARGS = {
+  top: {
+    key: {
+      valid: [ 'average', 'variance', 'deviation', 'forecast', 'messages', ],
+      default: 'average',
+  },
+    take: {
+      valid: {min: 0, max: 256},
+      default: 10,
+    },
+  },
+}
+
+receiver.router.get(`/api/convos/top`, cors(), async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    let arg_key: string
+    if (ARGS.top.key.valid.includes(req.query.key as string)) { arg_key = req.query.key as string }
+    else { arg_key = ARGS.top.key.default }
+
+    let arg_take: number
+    if (req.query.take) {
+      const _arg_take = parseInt(req.query.take as string) || ARGS.top.take.default
+      arg_take = Math.min(Math.max(_arg_take, ARGS.top.take.valid.min), ARGS.top.take.valid.max);
+    }
+    else { arg_take = ARGS.top.take.default }
+
+    const tops = await prisma.movingAverage.findMany({
+      distinct: ['slack_id'],
+      select: { average: true, variance: true, deviation: true, forecast: true, slack_id: true, created: true, },
+      where: {average: {not: 0}, variance: {not: 0}, deviation: {not: 0}, forecast: {not: 0}, },
+      orderBy: {created: 'desc'},
+    })
+
+    if (tops.length < 1) {
+      res.status(404).json({err: {code: 404, message: `empty resources`}})
+    } else {
+      tops.sort((x, y) => { return x[arg_key].toNumber() - y[arg_key].toNumber() })
+      res.status(200).json(tops.slice(0, arg_take))
+    }
+
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({err: {code: 500, message: `error getting resources`}})
+  }
+})
+
