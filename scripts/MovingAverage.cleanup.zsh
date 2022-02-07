1629f36bfb1dedddbcaf00a3a031ac5ea9698eed
Zachary Fogg <me@zfo.gg>
2022-02-06 19:38:34 -0500

    better cleanup i guess maybe. it will lock the db tho

--- scripts/MovingAverage.cleanup.zsh
+++ scripts/MovingAverage.cleanup.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env zsh -e
 
 local sdir="${0:a:h}"
 
@@ -8,14 +8,17 @@ local timeAgo="`date --date='45 days ago' -u +'%Y-%m-%d'`"
 
 local scriptSelect="$sdir"/sql/MovingAverage.SELECT.created_ASC_LIM_2.sql
 local scriptDelete="$sdir"/sql/MovingAverage.DELETE_550k.sql
+local scriptClean="$sdir"/sql/DATABASE.clean.sql
 
-local psqlArgs=("--quiet" "--tuples-only")
+local psqlArgs=(
+	"--quiet"
+	"--tuples-only"
+	"--echo-queries"
+)
 
 for i in $(seq 0 256); do
-    set -x
     local select1=`psql "${psqlArgs[@]}" -f "$scriptSelect" | head -n1`
     local latestDate=`echo "$select1" | cut -f2 -d'|' | grep -oe '[0-9-]\+' | head -n1`
-    set +x
     if [[ $latestDate > $timeAgo ]]; then
         echo "DONE :)! $latestDate > $timeAgo"
         exit 0
@@ -27,3 +30,5 @@ for i in $(seq 0 256); do
         psql "${psqlArgs[@]}" -f "$scriptDelete"
     done
 done
+
+psql "${psqlArgs[@]}" -f "$scriptClean"
